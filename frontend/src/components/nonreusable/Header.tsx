2391907bc87964f08314f17363f6e8a56b531208
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-06-13 15:26:32 +0530

    Initial commit: Project setup.

---
+++ frontend/src/components/nonreusable/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex flex-row items-center justify-between p-2">
+        <div>Leddit</div>
+        <div>Theme</div>
+      </header>
+    </>
+  )
+}
+
+export default Header
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
